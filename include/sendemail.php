347e437e6c83c0d541813b21805fe29ab8ec2d97
howardreith <howard.reith@gmail.com>
2018-10-12 09:56:22 -0400

    Attempted fix of contact form
    -Contact form should work. Will need to test on live website.

--- include/sendemail.php
+++ include/sendemail.php
@@ -5,8 +5,8 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'username@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'EdwardEaton@ereaton.com', // Your Email Address
+				'name' => 'Edward R. Eaton' // Your Name
 			);
 
 // Form Processing Messages
